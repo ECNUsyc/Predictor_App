8be37e04232bba485901eb5c284396ecdcd83c53
ECNUsyc <150006666+ECNUsyc@users.noreply.github.com>
2025-02-24 15:33:51 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -5,35 +5,50 @@ import xgboost as xgb
 import numpy as np
 import pandas as pd
 
-# load Pre-Trained XGBoost Model
+# 加载训练好的 XGBoost 模型
 model = pickle.load(open('xgb_model.pkl', 'rb'))
 
-st.title('Metal Doping MnO2 Specific Capacity Predictor')
+st.title('Metal-doped MnO2 Specific Capacity Predictor')
 # 读取元素性质数据
 atom_data = pd.read_csv('Atom.csv')
 # 用户选择元素
 element_symbols = atom_data.iloc[:, 0].tolist()  # 获取元素符号列表
 selected_element = st.selectbox('Select Element:', element_symbols)
-element_properties = atom_data[atom_data.iloc[:, 0] == selected_element].iloc[0, 1:4]
-MW, EN, RC = element_properties
+element_properties = atom_data[atom_data.iloc[:, 0] == selected_element].iloc[0, 1:]
+CG, MW, EN, CR = element_properties
 
 # 其他输入特征
 Ratio = st.number_input('Ratio', value=0.01, format="%.3f", step=0.001)  # 比例
-Zn2_plus = st.number_input('Zn2+', value=2.0,  step=0.5)  # Zn2+浓度
-st.write('Unit: mol/L')  # Zn2+的单位
-Mn2_plus = st.number_input('Mn2+', value=0.2, step=0.05)  # Mn2+浓度
-st.write('Unit: mol/L')  # Mn2+的单位
-Potential_Windows = st.number_input('Potential Windows', value=0.8,  step=0.05)  # 电压窗口
-st.write('Unit: V')  # 电压窗口的单位
-Current_Density = st.number_input('Current Density', value=0.5, format="%.2f", step=0.05)  # 电流密度
-st.write('Unit: A/g')  # 电流密度的单位
+Zn2_plus = st.number_input('Zn2+ concentration (Unit: mol/L)', value=2.0,  step=0.5)  # Zn2+浓度
+Mn2_plus = st.number_input('Mn2+ concentration (Unit: mol/L)', value=0.2, step=0.05)  # Mn2+浓度
+high_voltage = st.number_input('High Voltage (Unit: V)', value=1.8,  step=0.05)  # 电化学测试中的电压较高值
+low_voltage = st.number_input('Low Voltage (Unit: V)', value=0.8,  step=0.05)  # 电化学测试中的电压较低值
+Current_Density = st.number_input('Current Density (Unit: A/g)', value=0.5, format="%.2f", step=0.05)  # 电流密度
 
 # 显示选定元素的属性
 st.write(f'Selected Element: {selected_element}')
-st.write(f'Molecular Weight: {MW}, Electronegativity: {EN}, Covalent Radius: {RC}')
+st.write(f'Category: {CG}, Molecular weight: {MW}, Electronegativity: {EN}, Covalent Radius: {CR}')
 
 # 当用户点击预测按钮时进行模型预测
 if st.button('Predict'):
-    input_features = [[MW, EN, RC, Ratio, Zn2_plus, Mn2_plus, Potential_Windows, Current_Density]]  # 替换为实际特征名
-    prediction = model.predict(input_features)
+
+    # 转换为DataFrame并指定列名（需与训练时完全一致）
+    feature_columns = [
+        'MW', 'EN', 'CR','CG',
+        'Ratio', 'Zn2+', 'Mn2+',
+        'Low','High',  'CD'
+    ]
+    
+    input_df = pd.DataFrame([[
+        MW, EN, CR, CG,
+        Ratio,
+        Zn2_plus,
+        Mn2_plus,
+        low_voltage,
+        high_voltage,
+        Current_Density
+    ]], columns=feature_columns)
+    
+    prediction = model.predict(input_df)
     st.write(f'Predicted Specific Capacity: {prediction[0]:.4f} mAh/g')
+
