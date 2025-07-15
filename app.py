857a9c3be05dd93b1836563ab8857e3997e9a57d
ECNUsyc <150006666+ECNUsyc@users.noreply.github.com>
2025-07-15 15:46:12 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -4,20 +4,6 @@ import xgboost as xgb
 import numpy as np
 import pandas as pd
 import time
-st.markdown(
-    """
-    <style>
-    @import url('https://fonts.googleapis.com/css2?family=Lobster&display=swap');
-    
-    .stTitle {
-        font-family: 'Lobster', cursive;
-        font-size: 40px;
-        color: #FF6347;  /* Tomato color */
-        text-align: center;
-    }
-    </style>
-    """, unsafe_allow_html=True
-)
 # load the pre-trained model
 model = pickle.load(open('xgb_model1.pkl', 'rb'))
 
@@ -50,11 +36,11 @@ EM = Experiment_method[Experiment_method.iloc[:, 0] == selected_method].iloc[0, 
 # Other features with sliders
 st.subheader('Input Parameters')
 Ratio = st.slider('Ratio', min_value=0.01, max_value=1.0, value=0.01, step=0.001)
-Zn2_plus = st.slider('Zn2+ Concentration (mol/L)', min_value=0.0, max_value=4.0, value=2.0, step=0.1)
-Mn2_plus = st.slider('Mn2+ Concentration (mol/L)', min_value=0.0, max_value=1.0, value=0.2, step=0.05)
-high_voltage = st.slider('High Voltage (V)', min_value=0.0, max_value=2.0, value=1.8, step=0.01)
-low_voltage = st.slider('Low Voltage (V)', min_value=0.0, max_value=2.0, value=0.8, step=0.01)
-Current_Density = st.slider('Current Density (A/g)', min_value=0.0, max_value=5.0, value=0.5, step=0.01)
+Zn2_plus = st.slider('Zn2+ Concentration (mol/L)', min_value=0.1, max_value=4.0, value=2.0, step=0.1)
+Mn2_plus = st.slider('Mn2+ Concentration (mol/L)', min_value=0.05, max_value=1.0, value=0.2, step=0.01)
+high_voltage = st.slider('High Voltage (V)', min_value=1.25, max_value=1.95, value=1.8, step=0.01)
+low_voltage = st.slider('Low Voltage (V)', min_value=0.1, max_value=1.1, value=0.8, step=0.01)
+Current_Density = st.slider('Current Density (A/g)', min_value=0.1, max_value=5.0, value=0.5, step=0.01)
 
 # Prediction button with progress bar
 if st.button('Predict'):
